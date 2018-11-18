01498d54cdc348dbe12aa0e3d0d7cc393df38a69
Brian <riceb53@gmail.com>
2018-11-18 12:25:59 -0600

    connect api to home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="person in people">
+      <h3>{{ person.name }}</h3>
+      <h4>{{ person.bio }}</h4>
+      <hr>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require('axios');
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      people: [
+        {
+          name: "Andy",
+          bio: "my name is andy.",
+          bioVisible: true
+        },
+        {
+          name: "Alice",
+          bio: "she is a person.",
+          bioVisible: true
+        },
+        {
+          name: "Buck",
+          bio: "i like money.",
+          bioVisible: true
+        }
+      ]
+    };
+  },
+  created: function() {
+    console.log('in the created function');
+    console.log(this)
+    axios.get('http://localhost:3000/api/people').then(function(response) {
+      console.log(response.data);
+      this.people = response.data
+    }.bind(this))
+  },
+  methods: {},
+  computed: {}
+};
 </script>
