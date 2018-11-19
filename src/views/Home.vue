4a05e799c3778ed8e43d1a2470b2a06e2c781a20
Brian <riceb53@gmail.com>
2018-11-19 13:48:27 -0600

    reset for demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div v-for="person in people">
-      <h3>{{ person.name }}</h3>
-      <h4>{{ person.bio }}</h4>
-      <hr>
-    </div>
   </div>
 </template>
 
@@ -13,38 +8,14 @@
 </style>
 
 <script>
-var axios = require('axios');
 
 export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      people: [
-        {
-          name: "Andy",
-          bio: "my name is andy.",
-          bioVisible: true
-        },
-        {
-          name: "Alice",
-          bio: "she is a person.",
-          bioVisible: true
-        },
-        {
-          name: "Buck",
-          bio: "i like money.",
-          bioVisible: true
-        }
-      ]
     };
   },
   created: function() {
-    console.log('in the created function');
-    console.log(this)
-    axios.get('http://localhost:3000/api/people').then(function(response) {
-      console.log(response.data);
-      this.people = response.data
-    }.bind(this))
   },
   methods: {},
   computed: {}
