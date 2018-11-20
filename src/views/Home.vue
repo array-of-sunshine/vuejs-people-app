c66726216a94b6c4406d1172f2a46bf725536f46
Brian <riceb53@gmail.com>
2018-11-19 18:36:02 -0600

    add people input form and remove when button is clicked

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>{{ people.length }} people</h1>
+    <div v-for="person in people">
+      <h4>{{ person.name }}</h4>
+      <h3>{{ person.bio }}</h3>
+      <button v-on:click="removePerson(person)">Remove this person</button>
+      <hr>
+    </div>
+    <p>name:<input type="text" v-model="newPerson.name"></p>
+    <p>bio:<input type="text" v-model="newPerson.bio"></p>
+    <button v-on:click="addPerson()">add a new person</button>
   </div>
 </template>
 
@@ -13,11 +22,34 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
+      people: [
+        { name: "alice", bio: "my name is alice", bioVisible: true },
+        { name: "barney", bio: "i like to go to the beach", bioVisible: true },
+        { name: "charlie", bio: "i like checkers", bioVisible: true }
+      ],
+      newPerson: { name: "darlene", bio: "my name is darlene", bioVisible: true }
     };
   },
   created: function() {
   },
-  methods: {},
+  methods: {
+    addPerson: function() {
+      console.log('add the person');
+      this.people.push(this.newPerson);
+    },
+    removePerson: function(inputPerson) {
+      console.log('remoing the person');
+      console.log(inputPerson);
+      // find where inputPerson is
+      var index = this.people.indexOf(inputPerson);
+      // remove inputPerson
+      this.people.splice(index, 1);
+    }
+  },
   computed: {}
 };
+
+// click a button to run a function
+// that function adds a new person to the people array
+// user dynamic input to change values of person
 </script>
